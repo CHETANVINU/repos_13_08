bbc9368e0226c8b149d8a6d25de5adff1e71a151
chetan rodrigues <chetan_rodrigues@yahoo.co.in>
2020-08-13 08:29:45 +0000

    first_commint

---
+++ chetan_dev_proj/dbo/Tables/emp.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[emp] (
+    [visit_id]   INT          IDENTITY (1, 1) NOT NULL,
+    [first_name] VARCHAR (50) NOT NULL,
+    [last_name]  VARCHAR (50) NOT NULL,
+    [visited_at] DATETIME     NULL,
+    [phone]      VARCHAR (20) NULL,
+    [store_id]   INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([visit_id] ASC)
+);
+
